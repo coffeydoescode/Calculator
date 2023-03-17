4c6a0ae3774f128025579a495a1ef97fe566ae2b
coffeydoescode <seansince98@gmail.com>
2023-03-17 11:24:37 -0400

    Create Basic Operator Functions

---
+++ main.js
@@ -0,0 +1,23 @@
+function add(numb1, numb2) {
+  return numb1 + numb2;
+}
+
+console.log(add(91, 9));
+
+function subtract(numb1, numb2) {
+  return numb1 - numb2;
+}
+
+console.log(subtract(90, 1000));
+
+function multiply(numb1, numb2) {
+  return numb1 * numb2;
+}
+
+console.log(multiply(9, 1000));
+
+function divide(numb1, numb2) {
+  return numb1 / numb2;
+}
+
+console.log(divide(50, 10));
