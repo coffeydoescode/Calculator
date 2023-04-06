69642364d35de7b5f8261ffaa295f2c086fb2e22
coffeydoescode <seansince98@gmail.com>
2023-04-06 14:12:40 -0400

    Add Variables to Select Display

--- main.js
+++ main.js
@@ -4,34 +4,31 @@ function add(numb1, numb2) {
   return numb1 + numb2;
 }
 
-console.log(add(91, 9));
-
 function subtract(numb1, numb2) {
   return numb1 - numb2;
 }
-
-console.log(subtract(90, 1000));
 
 function multiply(numb1, numb2) {
   return numb1 * numb2;
 }
 
-console.log(multiply(9, 1000));
-
 function divide(numb1, numb2) {
   return numb1 / numb2;
 }
-
-console.log(divide(50, 10));
 
 function operate(operator, numb1, numb2) {
   return operator(numb1, numb2);
 }
 
-console.log(operate(subtract, 5, 3));
-
 let firstNumb;
 
-let mathOperator;
+let operator;
 
 let secondNumb;
+
+const display = document.getElementById("display");
+
+console.log(display);
+
+let displayValue = display.textContent;
+console.log(displayValue);
